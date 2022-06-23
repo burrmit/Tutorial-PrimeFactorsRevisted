4ea6c5c93bfc030211fdc0f0b6ef48bc1850e6a3
Mitchell Burr <burrmit@gmail.com>
2022-06-23 12:53:46 -0400

    Commiting the next set of tests for 2 and 3

--- prime.py
+++ prime.py
@@ -2,14 +2,42 @@
 Class for prime factors using iterable and generators.
 """
 
-class prime_factors():
+class prime_factors:
     """
     The prime factors class.
     """
     def __init__(self, num=0):
         """
-        The init method for defining the proper variables within the class.
+        The init method for defining the proper variables
+        within the class.
         """
-        if not isinstance(num, int):
-            raise ValueError
-        self.number = num
+        self.number = int(num)
+        self.factor = 2
+        self.factors = []
+
+    def __iter__(self):
+        """
+        This is the iter function that will allow the class to
+        be iterable.
+        """
+        return self
+
+    def __next__(self):
+        """
+        This method allows the class to continue to the next
+        iterator.
+        """
+        if self.number > 1:
+            while self.number % self.factor == 0:
+                self.factors.append(self.factor)
+                self.number = int(self.number / self.factor)
+
+            while self.number >= self.factor:
+                self.factor += 1
+                while self.number % self.factor == 0:
+                    self.factors.append(self.factor)
+                    self.number = int(self.number / self.factor)
+        else:
+            raise StopIteration
+
+        return self.factors
--- tests/test_prime.py
+++ tests/test_prime.py
@@ -17,3 +17,21 @@ def test_prime_factor_data_type():
     """
     with pytest.raises(ValueError):
         prime_factors('somestring')
+
+def test_1_returns_empty_string():
+    """
+    Test to ensure empty string when 1 is passed to it.
+    """
+    assert [number for number in prime_factors(1)] == []
+
+def test_2_returns_2():
+    """
+    Test that passing 2 returns 2.
+    """
+    assert [number for number in prime_factors(2)] == [[2]]
+
+def test_3_returns_3():
+    """
+    Test that 3 returns 3.
+    """
+    assert [number for number in prime_factors(3)] == [[3]]
