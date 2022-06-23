1f8dbbdf6605ae5fbaff4321309386a47f069235
Mitchell Burr <burrmit@gmail.com>
2022-06-23 12:06:00 -0400

    Commiting the first test for class

--- prime.py
+++ prime.py
@@ -2,8 +2,14 @@
 Class for prime factors using iterable and generators.
 """
 
-class PrimeFactors():
+class prime_factors():
     """
     The prime factors class.
     """
-    pass
+    def __init__(self, num=0):
+        """
+        The init method for defining the proper variables within the class.
+        """
+        if not isinstance(num, int):
+            raise ValueError
+        self.number = num
--- tests/test_prime.py
+++ tests/test_prime.py
@@ -1,12 +1,19 @@
 
 
 import pytest
-from prime import PrimeFactors
+from prime import prime_factors
 
 
 def test_prime_class():
     """
     Testing the class is imported and callable.
     """
-    prime = PrimeFactors()
+    prime = prime_factors()
     assert prime
+
+def test_prime_factor_data_type():
+    """
+    Test that data type is only int or raise ValueError.
+    """
+    with pytest.raises(ValueError):
+        prime_factors('somestring')
